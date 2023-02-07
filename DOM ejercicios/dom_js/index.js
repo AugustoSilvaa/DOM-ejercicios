105737b91a12bc2fbfcfb857dff00df5949bc44e
Xavier Silva <xavier.a.silva.b@gmail.com>
2023-02-07 15:16:27 -0400

    integración de sección 5 Responsive Tester

--- DOM ejercicios/dom_js/index.js
+++ DOM ejercicios/dom_js/index.js
@@ -6,6 +6,7 @@ import countdown from "./cuentaRegresiva.js";
 import scrollTopBotton from "./botton_scroll.js";
 import darkTheme from "./tema_oscuro.js";
 import responsiveMedia from "./objeto_responsive.js";
+import responsiveTester from "./prueba_responsive.js";
 
 document.addEventListener("DOMContentLoaded", e=>{
     hamburgerMenu(".panel-btn",".panel", ".menu a");
@@ -15,6 +16,7 @@ document.addEventListener("DOMContentLoaded", e=>{
     scrollTopBotton(".scroll-top-btn");
     responsiveMedia("youtube","(min-width:1024px)",`<a href="https://www.youtube.com/watch?v=2SetvwBV-SU&list=PLvq-jIkSeTUZ6QgYYO3MwG9EMqC-KoLXA" target="_blank" class="a-responsive">Ver video</a>`, `<iframe width="560" height="315" src="https://www.youtube.com/embed/2SetvwBV-SU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`);
     responsiveMedia("gmaps","(min-width:1024px)",`<a href="https://goo.gl/maps/RE2F4Pn9VcugCbCL6" target="_blank" class="a-responsive">Ver Mapa</a>`, `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1881.3307860565512!2d-99.16869369208443!3d19.427023126223627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff35f5bd1563%3A0x6c366f0e2de02ff7!2sEl%20%C3%81ngel%20de%20la%20Independencia!5e0!3m2!1ses-419!2sve!4v1675784297589!5m2!1ses-419!2sve" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`);
+    responsiveTester("responsive-tester");
     
 })
 
---
+++ DOM ejercicios/dom_js/prueba_responsive.js
@@ -0,0 +1,18 @@
+
+export default function responsiveTester(form){
+    const $form=document.getElementById(form);
+    let tester;
+
+    document.addEventListener("submit", (e)=>{
+        if (e.target === $form) {
+            e.preventDefault();
+            //alert("formulario enviado")
+            tester=window.open($form.direccion.value,"tester",`innerWidth=${$form.ancho.value}, innerHeight=${$form.alto.value}`)
+            
+        }
+    })
+Document.addEventListener("click", (e)=>{
+    if (e.target===$form.cerrar) tester.close();
+})
+
+}
